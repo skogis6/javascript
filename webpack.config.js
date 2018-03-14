d5d776722fe1783320e894998a70868ca8be52f0
David <skogis6@gmail.com>
2018-03-14 12:02:15 +0100

    Nu med lint och less

--- webpack.config.js
+++ webpack.config.js
@@ -5,13 +5,19 @@ module.exports = {
     mode: 'development',
     entry: './code.js',
     output: {
-        path: __dirname,
+        path: path.resolve(__dirname, "distribution"),
         filename:'bundle.js'
     },
+
     devServer: {
-        contentBase: __dirname, // the root for the server
+        contentBase: path.resolve(__dirname, "distribution"), // the root for the server
         watchContentBase: true, // so we reload if other stuff like CSS changes
         port: 9000, // it'll now be at http://localhost:9000
         
-    }
+    },
+    plugins: [
+        new WebpackNotifierPlugin({
+            alwaysNotify:true
+        })
+    ]
 };
