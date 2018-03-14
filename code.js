5dd280805a1e03048a1010d71a88d80f3b1780fa
David <skogis6@gmail.com>
2018-03-14 11:35:30 +0100

    dag två klar

--- code.js
+++ code.js
@@ -70,5 +70,5 @@ function renderMovie(movie){
         //console.log("wth", rating)
         changeStarRating(rating);
     });
-
+// alert("Hej")
 renderMovie(movieData);
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,17 @@
 let path = require("path");
+const WebpackNotifierPlugin = require("webpack-notifier");
 
 module.exports = {
-mode: 'development',
-entry: './code.js',
-output: {
-path: path.resolve(__dirname,"."),
-filename:'bundle.js'
-}
+    mode: 'development',
+    entry: './code.js',
+    output: {
+        path: __dirname,
+        filename:'bundle.js'
+    },
+    devServer: {
+        contentBase: __dirname, // the root for the server
+        watchContentBase: true, // so we reload if other stuff like CSS changes
+        port: 9000, // it'll now be at http://localhost:9000
+        
+    }
 };
