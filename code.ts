59bac902f55f6c35d62736d9d0f765b6c3ae36f1
David <skogis6@gmail.com>
2018-03-14 15:55:00 +0100

    Uppdaterat med TypeScript jQuery

--- code.ts
+++ code.ts
@@ -1,77 +1,83 @@
 // let movieData = require("./data")
 // let $ = require("jquery")
-
-
+import * as $ from "jquery";
 import {movieData} from "./data";
-import * as $ from "jquery"; 
+import {Review} from "./interfaces";
 
-function renderMovie(movie){
+// function renderMovie(movie) {
+function renderMovie(data: Review) {
+// document.getElementById("movietitle").innerText =movie.title;
+// document.getElementById("synopsis").innerText = movie.synopsis;
+// document.getElementById("director").innerText = movie.director;
+// document.getElementById("poster").setAttribute("src",movie.imgUrl);
 
-    
-    // document.getElementById("movietitle").innerText =movie.title;
-    // document.getElementById("synopsis").innerText = movie.synopsis;
-    // document.getElementById("director").innerText = movie.director;
-    // document.getElementById("poster").setAttribute("src",movie.imgUrl);
+/*--------------------------------*/
+// Här kommer JQuerry
+$(".titelinfo h1").text(data.title);
+$(".titelinfo p").text(data.synopsis);
+$(".container img").attr("src", data.imgUrl);
 
-    /*--------------------------------*/
-    // Här kommer JQuerry
-    $(".titelinfo h1").text(movie.title);
-    $(".titelinfo p").text(movie.synopsis);
-    $(".container img").attr("src",movie.imgUrl)
-    
-    //let actorList ="";
-    //     for(let i=0; i< movie.actors.length;i++){
-    //         actorList += "<li>"+movie.actors[i]+"</li>";
-    //     } 
-    //  document.getElementById("actors").innerHTML = actorList;
+// let actorList ="";
+//     for(let i=0; i< movie.actors.length;i++){
+//         actorList += "<li>"+movie.actors[i]+"</li>";
+//     }
+//  document.getElementById("actors").innerHTML = actorList;
+/*--------------------------------*/
+// Här kommer JQuerry
+$(".titelinfo ul").empty();
 
-    /*--------------------------------*/
-    // Här kommer JQuerry
-    $(".titelinfo ul").empty();
-    for(let i=0; i<movie.actors.length; i++){
-    $(".titelinfo ul").append("<li>"+movie.actors[i]+"</li>");
+/*--------------------------------*/
+// Här kommer en vanlig for loop
+// for (let i = 0; i < data.actors.length; i++) {
+// $(".titelinfo ul").append("<li>" + data.actors[i] + "</li>");
+// }
+
+/*--------------------------------*/
+// Forloopen kan ersättas med denna kod
+for (const actor of data.actors) {
+    $(".titelinfo ul").append("<li>" + actor + "</li>");
     }
 }
 
-    //function changeStarRating(rating){
-    // for(let i =1; i <=5;i++){
-    //     let star = document.getElementById("star"+i);
-    //     if(i<=rating){
-    //         star.classList.add("filled");
-    //     }
-    //     else{
-    //         star.classList.remove("filled");
-    //     }
-    // }
+// function changeStarRating(rating){
+// for(let i =1; i <=5;i++){
+//     let star = document.getElementById("star"+i);
+//     if(i<=rating){
+//         star.classList.add("filled");
+//     }
+//     else{
+//         star.classList.remove("filled");
+//     }
+// }
 
-    /*--------------------------------*/
-    // Här kommer JQuerry
-    function changeStarRating(rating){
-        //console.log("CHANGE", rating)
-     $(".filled").removeClass("filled");
-        for(let i=1;i<=rating;i++){
-        $("#"+i).addClass("filled")
-        //$(".stars :nth-child (-n+"+rating+")").addClass("filled");<!-- denna har fel i sig-->
-     }
+/*--------------------------------*/
+// Här kommer JQuerry
+function changeStarRating(rating) {
+    // console.log("CHANGE", rating)
+    $(".filled").removeClass("filled");
+    for (let i = 1 ; i <= rating; i++) {
+    $("#" + i).addClass("filled");
+    // $(".stars :nth-child (-n+"+rating+")").addClass("filled");<!-- denna har fel i sig-->
     }
+}
 
-    //-------------------------------------------------
+// -------------------------------------------------
 
-    // for(let i=1;i<=5;i++){
-    //     let star = document.getElementById("star"+i);
-    //         star.addEventListener("click",function(){
-    //             changeStarRating(i);
-    //         });  
-    // }
+// for(let i=1;i<=5;i++){
+//     let star = document.getElementById("star"+i);
+//         star.addEventListener("click",function(){
+//             changeStarRating(i);
+//         });
+// }
 
-    /*--------------------------------*/
-    // Här kommer JQuerry
-    $(".stars").on("click","span",function(e){
-        let star = $(e.target);
-        let rating = parseInt(star.attr("id"));
-        //let rating = Array.from(e.target.parentElement.children).indexOf(e.target)+1;<!-- denna har fel i sig-->
-        //console.log("wth", rating)
-        changeStarRating(rating);
-    });
+/*--------------------------------*/
+// Här kommer JQuerry
+$(".stars").on("click", "span", (e) => {
+const star = $(e.target);
+const rating = parseInt(star.attr("id"));
+// let rating = Array.from(e.target.parentElement.children).indexOf(e.target)+1;<!-- denna har fel i sig-->
+// console.log("wth", rating)
+changeStarRating(rating);
+});
 // alert("Hej")
 renderMovie(movieData);
---
+++ interfaces.ts
@@ -0,0 +1,7 @@
+export type Review = {
+    title: string
+    imgUrl: string
+    synopsis: string
+    director: string
+    actors: string[],
+  };
